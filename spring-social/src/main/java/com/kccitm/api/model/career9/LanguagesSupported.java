6decec9a7d3704b66c68afd900ea9c42cdef59b7
isthatdhruv <isthatdhruvv@gmail.com>
2025-09-17 17:03:48 +0530

    tested api calls for language create , language question create

--- spring-social/src/main/java/com/kccitm/api/model/career9/LanguagesSupported.java
+++ spring-social/src/main/java/com/kccitm/api/model/career9/LanguagesSupported.java
@@ -1,6 +1,7 @@
 package com.kccitm.api.model.career9;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -12,11 +13,14 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 
 @Entity
 @Table(name="Language_Table")
 public class LanguagesSupported implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,46 +28,56 @@ public class LanguagesSupported implements Serializable {
 
     private String languageName;
 
-    @OneToMany(cascade= CascadeType.ALL)
-    @JoinColumn(name="fk_LanguageQuestion", referencedColumnName = "languageId")
-    private List<LanguageQuestion> LanguageQuestion;
+    @OneToMany(mappedBy = "language", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<LanguageQuestion> questions = new ArrayList<>();
 
-    @OneToMany(cascade= CascadeType.ALL)
-    @JoinColumn(name="FK_LanguageOption", referencedColumnName = "languageId")
-    private List<LanguageOption> LanguageOption;
+    // @OneToMany(cascade= CascadeType.ALL)
+    // @JoinColumn(name="fk_LanguageQuestion", referencedColumnName = "languageId")
+    // private List<LanguageQuestion> LanguageQuestion;
+
+    // @OneToMany(cascade= CascadeType.ALL)
+    // @JoinColumn(name="FK_LanguageOption", referencedColumnName = "languageId")
+    // private List<LanguageOption> LanguageOption;
 
     //getter/setter
-    public List<LanguageOption> getLanguageOption(){
-        return LanguageOption;
-    }
+    // public List<LanguageOption> getLanguageOption(){
+    //     return LanguageOption;
+    // }
 
-    public void setLanguageOption(List<LanguageOption> LanguageOption){
-        this.LanguageOption = LanguageOption;
-    }
+    // public void setLanguageOption(List<LanguageOption> LanguageOption){
+    //     this.LanguageOption = LanguageOption;
+    // }
 
-    public List<LanguageQuestion> getLanguageQuestion(){
-        return LanguageQuestion;
-    }
+    // public List<LanguageQuestion> getLanguageQuestion(){
+    //     return LanguageQuestion;
+    // }
 
-    public void setLanguageQuestion(List<LanguageQuestion> LanguageQuestion){
-        this.LanguageQuestion = LanguageQuestion;
-    }
+    // public void setLanguageQuestion(List<LanguageQuestion> LanguageQuestion){
+    //     this.LanguageQuestion = LanguageQuestion;
+    // }
 
     //getter setter
-     public Long getLangId() {
-        return languageId;
+    public List<LanguageQuestion> getQuestions() {
+        return questions;
     }
-
-    public void setId(Long languageId) {
-        this.languageId = languageId;
+    public void setQuestions(List<LanguageQuestion> questions) {
+        this.questions = questions;
     }
-
-    public String getLangName() {
+    public String getLanguageName() {
         return languageName;
     }
 
-    public void setLangName(String languageName) {
+    public void setLanguageName(String languageName) {
         this.languageName = languageName;
+    }
+
+    public Long getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Long languageId) {
+        this.languageId = languageId;
     }
     
 }
