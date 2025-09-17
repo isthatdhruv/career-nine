cce824b21b2334841a4c7deb801cc0d980c3d35e
Shivangi Bhartiya <shivangibhartiyaa@gmail.com>
2025-09-17 11:48:01 +0530

    created languageOptionsController, LanguageSupportedController

--- spring-social/src/main/java/com/kccitm/api/repository/Career9/LanguageOptionRepository.java
+++ spring-social/src/main/java/com/kccitm/api/repository/Career9/LanguageOptionRepository.java
@@ -3,9 +3,9 @@ package com.kccitm.api.repository.Career9;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.kccitm.api.model.career9.LanguagesSupported;
+import com.kccitm.api.model.career9.LanguageOption;
 
 @Repository
-public interface LanguageOptionRepository extends JpaRepository<LanguagesSupported, Long>{
+public interface LanguageOptionRepository extends JpaRepository<LanguageOption, Long>{
     
 }
