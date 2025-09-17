df2107449d83d669a157b872989e364c869a90a9
hibaasameer-beep <hibaasameer@gmail.com>
2025-09-17 11:47:44 +0530

    created languageQuestionController

--- spring-social/src/main/java/com/kccitm/api/repository/Career9/LanguageQuestionRepository.java
+++ spring-social/src/main/java/com/kccitm/api/repository/Career9/LanguageQuestionRepository.java
@@ -1,12 +1,15 @@
 package com.kccitm.api.repository.Career9;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.kccitm.api.model.career9.LanguagesSupported;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.kccitm.api.model.career9.LanguageQuestion;
+// import com.kccitm.api.model.career9.LanguagesSupported;
 
 
 @Repository
-public interface LanguageQuestionRepository extends JpaRepository<LanguagesSupported, Long>{
+public interface LanguageQuestionRepository extends JpaRepository<LanguageQuestion, Long>{
+
+    // LanguageQuestion save(LanguageQuestion languageQuestion);
     
 }
