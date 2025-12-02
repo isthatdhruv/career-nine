b793a3abdb3c002396827626aa3dd17009a31e33
ShivangiBhartiya <shivangibhartiyaa@gmail.com>
2025-12-02 12:45:22 +0530

    some changes

--- spring-social/src/main/java/com/kccitm/api/model/Gender.java
+++ spring-social/src/main/java/com/kccitm/api/model/Gender.java
@@ -3,6 +3,8 @@ package com.kccitm.api.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -17,7 +19,7 @@ public class Gender implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	// @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String type;
