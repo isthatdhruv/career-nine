f717112dd2b56cddfb7aad92b7edf1901996d1fd
isthatdhruv <isthatdhruvv@gmail.com>
2026-01-15 15:41:31 +0530

    sir ne changes kiye boht saare

--- spring-social/src/main/java/com/kccitm/api/repository/Career9/UserStudentRepository.java
+++ spring-social/src/main/java/com/kccitm/api/repository/Career9/UserStudentRepository.java
@@ -1,9 +1,11 @@
 package com.kccitm.api.repository.Career9;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kccitm.api.model.career9.UserStudent;
 
 public interface UserStudentRepository extends JpaRepository<UserStudent, Long> {
-
+    Optional<UserStudent> getByUserId(Long userId);
 }
