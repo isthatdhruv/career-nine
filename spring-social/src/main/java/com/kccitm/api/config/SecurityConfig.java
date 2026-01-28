23036c938f153b99ac6999777bd21529b9e5c6ec
Utkrisht <utkrisht.mittal@career-9.com>
2026-01-28 10:23:22 +0000

    Production Configration Changes

--- spring-social/src/main/java/com/kccitm/api/config/SecurityConfig.java
+++ spring-social/src/main/java/com/kccitm/api/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "http://localhost:3000",
                 "http://192.168.3.78:3000",
                 "http://192.168.0.204:3000",
-                "https://192.168.0.204:3000"));
+                "https://192.168.0.204:3000","https://dashboard.career-9.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
